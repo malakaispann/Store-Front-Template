1d3ea29b4d44e88f8299ca2fdd73b33913954f91
Malakai Spann <malakaispann@gmail.com>
2025-09-07 12:03:22 -0400

    chore(links): update github references

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import {default as gitLogo} from '../assets/github-logo.png';
 function Footer() {
   return (
       <div className='absolute bottom-0 h-16 w-screen bg-zinc-100 border-t-2'>
-              <a href='https://github.com/KayDVC' className='flex w-full h-full justify-center items-center text-sm hover:bg-white'>
+              <a href='https://github.com/malakaispann' className='flex w-full h-full justify-center items-center text-sm hover:bg-white'>
                     2022 @KayDVC 
                   <img src={gitLogo} className='h-6' alt='Link to my GitHub account.' />
               </a>
